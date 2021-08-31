47bc1f3116654302a3b994b88d3d7c5a90be1431
ElmerMarin <elmermarindc@gmail.com>
2021-08-31 11:47:36 -0500

    agregando vehiculos

---
+++ Sistem-Vehiculos API/Data/Entities/DataContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Sistem_Vehiculos_API.Data.Entities;
+
+namespace Sistem_Vehiculos_API.Data.Entities
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+        }
+
+        public DbSet<VehicleType> VehicleTypes { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehicleType>().HasIndex(x => x.Descripcion).IsUnique();
+
+        }
+
+
+
+
+    }
+}
